736dc2d30f50d75bbe2d04461f52bc0cd7ffd23c
Buğracan Osman Aytaç <blg.bugra@hotmail.com>
2018-12-25 23:36:40 +0300

    Add files via upload

---
+++ Arduino Koşu Projesi/Ko_u_Arduino/Ko_u_Arduino.ino
@@ -0,0 +1,76 @@
+int Laser = 12;
+int Detector = 13;
+int Laser2 =11;
+int Detector2 = 10;
+unsigned long start=0;
+unsigned long bitis=0;
+
+ 
+
+
+void setup() {
+
+  
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(Laser, OUTPUT);
+pinMode(Detector, INPUT);
+pinMode(Laser2, OUTPUT);
+pinMode(Detector2,INPUT);
+}
+void a(){
+  
+boolean val=digitalRead(Detector);
+boolean val2=digitalRead(Detector2);
+ 
+while(val2==0 && val==0){
+val2=digitalRead(Detector2);
+val=digitalRead(Detector);
+  }
+
+
+  }
+
+  
+void b(){
+  
+boolean val3=digitalRead(Detector);
+boolean val4=digitalRead(Detector2);
+
+  if(val3!=1 && val4!=0)
+  {
+    val4=digitalRead(Detector2);
+    val3=digitalRead(Detector);
+    b();
+    }
+
+  }
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(Laser , HIGH);
+digitalWrite(Laser2 , HIGH);
+
+
+//delay(100);
+
+
+start=millis();
+
+a();
+bitis=millis();
+
+if(bitis>start){
+  if(bitis-start>100){
+    Serial.println(bitis-start);
+    /*Serial.println("*");*/
+    
+  }
+
+
+}
+b();
+/*if(bitis-start>100)*/
+/*Serial.println(bitis-start);*/
+  }
+
+
---
+++ Arduino Yukseklık Projesı/yuksekl_k/yuksekl_k.ino
@@ -0,0 +1,55 @@
+ const int trig=3;
+ const int echo=2;
+int Laser = 12;
+int Detector = 13;
+
+int mesafe=0;
+int sure =0 ;
+int son = 0;
+
+void setup() {
+          pinMode(trig,OUTPUT);
+          pinMode(echo,INPUT);
+          pinMode(Laser, OUTPUT);
+          pinMode(Detector, INPUT);
+          Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void a(int d){
+  
+  if(d!=0)
+  Serial.println(String(d));
+  }
+void loop() {
+  digitalWrite(Laser , HIGH);
+  boolean val=digitalRead(Detector);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+  sure=pulseIn(echo,HIGH);
+  mesafe= (sure/2) / 28.5; 
+
+  /*Serial.println(String(mesafe)); */
+  if(mesafe>son)
+  {
+
+ 
+  if(mesafe<40 && mesafe>10) {
+  son=mesafe;}
+  a(son);
+  if(val==1)
+  {
+    delay(5000);
+    son=0;
+    }
+  /*if(son>70)
+  Serial.end();*/
+  }
+  
+ 
+  // put your main code here, to run repeatedly:
+
+}
